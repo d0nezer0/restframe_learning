9f49066a92255ebcab3eb7c0caf13e312f06f08c
d0nezer0 <vincentchou@126.com>
2015-09-14 17:59:19 +0800

    Tutorial 2: Requests and Responses api with json or api format. and get Status codes

--- wangli_api/app/views.py
+++ wangli_api/app/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User, Group
+from rest_framework import status
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 from wangli_api.app.models import Snippet
 from wangli_api.app.serializers import UserSerializer, GroupSerializer, SnippetSerializer
 
@@ -34,28 +37,26 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def snippet_list(request):
     """
-    List all code snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @csrf_exempt
-# def snippet_detail(request, pk):
-def snippet_detail(request, pk, format=None):
+def snippet_detail(request, pk):
     """
     Retrieve, update or delete a code snippet.
     """
