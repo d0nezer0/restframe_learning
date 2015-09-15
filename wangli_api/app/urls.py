d79161966abe851eb6590fbae52f5e950ed2999e
d0nezer0 <vincentchou@126.com>
2015-09-15 15:46:41 +0800

    Tutorial 6: ViewSets & Routers

--- wangli_api/app/urls.py
+++ wangli_api/app/urls.py
@@ -9,6 +9,8 @@ router = routers.DefaultRouter()
 router.register(r'router/users', views.UserViewSet)
 router.register(r'router/groups', views.GroupViewSet)
 
+router.register(r'router/snippets', views.SnippetViewSet)
+
 urlpatterns += [
     url(r'^', include(router.urls)),
     # this is for user to login and logout
@@ -17,10 +19,10 @@ urlpatterns += [
 urlpatterns += [
     url(r'^$', views.api_root),
 
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
-
-    url(r'^app/snippets/$', views.SnippetList.as_view(), name='snippet-list'),
-    url(r'^app/snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
-    url(r'^app/snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    # url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    # url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+    #
+    # url(r'^app/snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    # url(r'^app/snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
+    # url(r'^app/snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
--- wangli_api/app/views.py
+++ wangli_api/app/views.py
@@ -13,6 +13,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework.decorators import detail_route
 
 
 @api_view(('GET',))
@@ -36,21 +37,12 @@ class SnippetHighlight(generics.GenericAPIView):
         return Response(snippet.highlighted)
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    This viewset automatically provides `list` and `detail` actions.
+    class UserViewSet(viewsets.ModelViewSet):   this contains all kinds of viewsets.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
@@ -72,22 +64,22 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
 
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                           IsOwnerOrReadOnly,)
 
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-                          IsOwnerOrReadOnly,)
-
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
